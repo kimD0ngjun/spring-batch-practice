f5d97c3725a6c3bc626207e258ba81d663151e90
kimD0ngjun <chickentasty0112@gmail.com>
2024-11-20 21:47:03 +0900

    feat: 옮기려는 데이터 테이블 엔티티 정의

---
+++ src/main/java/com/example/batchpractice/entity/BeforeEntity.java
@@ -0,0 +1,20 @@
+package com.example.batchpractice.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 옮기려는 데이터 출발 테이블
+ */
+@Entity
+@Getter
+@Setter
+public class BeforeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+}
