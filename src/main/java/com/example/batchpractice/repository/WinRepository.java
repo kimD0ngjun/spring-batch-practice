06529de9fbbe81236d651ca7703a1682c2d38d04
kimD0ngjun <chickentasty0112@gmail.com>
2024-11-21 13:58:38 +0900

    feat: 두 번째 엔티티 레포지토리 DAO 생성

---
+++ src/main/java/com/example/batchpractice/repository/WinRepository.java
@@ -0,0 +1,12 @@
+package com.example.batchpractice.repository;
+
+import com.example.batchpractice.entity.WinEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WinRepository extends JpaRepository<WinEntity, Long> {
+    Page<WinEntity> findByWinGreaterThanEqual(Long win, Pageable pageable);
+}
